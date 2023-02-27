80b534315c3768a7e1061bb4babc28557878fae3
SiBlue7 <enzo.chevalier@lacatholille.fr>
2023-02-27 18:03:32 +0100

    Ajout de la barre de recherche

--- ProjetInterfaceAPI/src/App.vue
+++ ProjetInterfaceAPI/src/App.vue
@@ -2,14 +2,38 @@
 export default {
   data() {
     return {
-      message: 'Hello world !'
+      
     }
   }
 }
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
+  <div class="app">
+    <header>
+      <h1>The<strong>Anime</strong>Database</h1>
+
+      <form class="search-box">
+        <input type="search"
+               class="search-field"
+               placeholder="Search for an anime..."
+               required>
+      </form>
+    </header>
+    <!--
+    <main>
+      <div class="cards" v-if="animelist.length > 0">
+        <Card
+            v-for="anime in animelist"
+            :key="anime.mal_id"
+            :anime="anime" />
+      </div>
+      <div class="no-results" v-else>
+        <h3>Sorry, we have no results...</h3>
+      </div>
+    </main>
+    -->
+  </div>
 </template>
 
 <style>

04b49488fcd826942d574865997bbe95d0981a55
Enzo C <enzo20112002@gmail.com>
2023-03-26 13:42:26 +0200

    Ajout des derniere modifs

--- ProjetInterfaceAPI/src/App.vue
+++ ProjetInterfaceAPI/src/App.vue
@@ -1,18 +1,21 @@
 <script>
 import axios from "axios";
+import Card from "@/components/Card.vue";
 
 export default {
+  components: {Card},
   data() {
     return {
       baseURL: 'https://api.mangadex.org',
-      title: 'Kanojyo to Himitsu to Koimoyou',
-      search_query,
-      mangaList,
-      getManga,
+      title: '',
+      cover_art: '',
+      search_query: '',
+      mangaList: [],
     }
   },
   mounted() {
     this.getManga();
+    //this.getMangaCover();
   },
 
   setup() {
@@ -21,17 +24,36 @@ export default {
 
   methods:{
     async getManga() {
-      const search_query = ref("");
-      const mangaList = ref([]);
       const resp = await axios({
         method: 'GET',
         url: this.baseURL+'/manga',
         params: {
-          title: search_query.value
+          // search_query: this.search_query
+          title: this.title
+        }
+      }).then(
+          (result) => {
+            console.log(result.data.data);
+            this.mangaList = result.data.data[0];
+            this.getMangaCover();
+          }
+      );
+    },
+
+    async getMangaCover() {
+      console.log(this.mangaList);
+      console.log(this.baseURL+'/manga/'+ this.mangaList.id + '&includes[]=cover_art');
+      const resp = await axios({
+        method: 'GET',
+        url: this.baseURL+'/manga/'+ this.mangaList.id + '?includes[]=cover_art',
+        params: {
+          // search_query: this.search_query
+          title: this.title
         }
       }).then(
           (result) => {
             console.log(result);
+            this.mangaList = [result];
           }
       );
     }
@@ -43,30 +65,30 @@ export default {
 <template>
   <div class="app">
     <header>
-      <h1>The<strong>Anime</strong>Database</h1>
+      <h1>The<strong>Manga</strong>Database</h1>
 
       <form class="search-box" @submit.prevent="getManga">
         <input type="search"
                class="search-field"
                placeholder="Search for a manga..."
                required
-               v-model="search_query"
+               v-model="title"
         />
       </form>
     </header>
-    <!--
+
     <main>
       <div class="cards" v-if="mangaList.length > 0">
         <Card
-            v-for="anime in mangaList"
-            :key="anime.mal_id"
-            :anime="anime" />
+            v-for="manga in mangaList"
+            :key="manga.data.data.id"
+            :manga="manga" />
       </div>
       <div class="no-results" v-else>
         <h3>Sorry, we have no results...</h3>
       </div>
     </main>
-    -->
+
   </div>
 </template>
 
